484a6546b31c54be7ce0af2c5f2550aa7c72d93f
roseb.04 <rose@neontribe.co.uk>
2023-08-16 13:57:02 +0100

    rework dark mode toggle

--- main.js
+++ main.js
@@ -1,27 +1,11 @@
-// night mode toggle
-const toggleSwitch = document.querySelector(
-  '.theme-switch input[type="checkbox"]'
-);
-const currentTheme = localStorage.getItem("theme");
-
-if (currentTheme) {
-  document.documentElement.setAttribute("data-theme", currentTheme);
-
-  if (currentTheme === "dark") {
-    toggleSwitch.checked = true;
-  }
-}
-
+const toggle = document.querySelector('.dark-mode input')
 function switchTheme(e) {
-  if (e.target.checked) {
-    document.documentElement.setAttribute("data-theme", "dark");
-    localStorage.setItem("theme", "dark");
-  } else {
-    document.documentElement.setAttribute("data-theme", "light");
-    localStorage.setItem("theme", "light");
-  }
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
 }
 
-toggleSwitch.addEventListener("change", switchTheme, false);
+toggle.addEventListener('change', switchTheme, false)
 
-console.log(currentTheme);
